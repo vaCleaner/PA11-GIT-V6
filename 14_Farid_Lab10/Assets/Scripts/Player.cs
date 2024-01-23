026d9637781a9db5b129d07c67badd842751860f
vaCleaner <155930075+vaCleaner@users.noreply.github.com>
2024-01-23 15:29:24 +0800

    done

---
+++ 14_Farid_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    private float objectWidth;
+    private float objectHeight;
+    public float speed;
+    public Camera MainCamera;
+    private Vector2 screenbound;
+    private float ObjectW;
+    private float ObjectH;
+    int score;
+    public Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenbound = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        ObjectW = transform.GetComponent<MeshRenderer>().bounds.extents.x; 
+        ObjectH = transform.GetComponent<MeshRenderer>().bounds.extents.y;
+        score = 0;
+        scoretext.text = "Score : " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+    private void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.y = Mathf.Clamp(viewPos.y, screenbound.y * -1 + ObjectH, screenbound.y - ObjectH);
+
+        transform.position = viewPos;
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+
+
+
+        }
+
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+
+            score += 1;
+            scoretext.text = "Score : " + score.ToString();
+           
+
+
+        }
+    }
+
+
+
+
+
+
+}
---
+++ 14_Farid_Lab10/Assets/Scripts/time.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class time : MonoBehaviour
+{
+    int score;
+    public Text scoretext;
+
+    private void Start()
+    {
+        score = 0;
+        scoretext.text = "Score : " + score.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+
+            score += 1;
+            scoretext.text = "Score : " + score.ToString();
+
+
+
+        }
+    }
+
+
+
+
+}
